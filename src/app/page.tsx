dd05b58bbb84de2120719d49fe72e478e8dc58fc
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2024-08-27 22:07:53 +0700

    Add input checkbox to include private ASN or not
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function DetailsPage() {
   const [data, setData] = useState<RBTHDetails[] | null>(null);
   const [search, setSearch] = useState<string>("");
   const [err, setErr] = useState<string | null>(null);
+  const [includePrivate, setIncludePrivate] = useState<boolean>(false);
 
   async function fetchData() {
     const res = await getRBTHData();
@@ -24,20 +25,27 @@ export default function DetailsPage() {
   return (
     <div className="my-[2vh] ml-2 mr-2 text-sm md:text-xl">
       <div className="mb-3 pb-1 border-b border-black">RBTH-1 MTen</div>
-      <div className="flex gap-2 mb-3 pb-2 border-b border-black">
-        Search
-        <input
-          disabled={data === null}
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="border border-black disabled:bg-[#f4f4f4]"
-          type="text"
-        />
+      <div className="flex flex-col gap-2 mb-3 pb-2 border-b border-black">
+        <div className="flex gap-2">
+          Search
+          <input
+            disabled={data === null}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="border border-black disabled:bg-[#f4f4f4]"
+            type="text"
+          />
+        </div>
+        <div className="flex gap-2">
+          <input onChange={(e) => setIncludePrivate(e.target.checked)} checked={includePrivate} type="checkbox" />
+          Include private?
+        </div>
       </div>
       {err ? (
         <div>Error! {err}</div>
       ) : data ? (
         data
+          .filter((d) => (!includePrivate ? !d.asn.toLowerCase().includes("private") : true))
           .filter((d) => {
             const isPrivate = d.asn.toLowerCase().includes("private");
 
